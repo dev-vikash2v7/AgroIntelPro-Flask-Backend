3bfec105b0646f5af143ee4c8fbe8de239dabde0
vikash verma <vikashvermacom92@gmail.com>
2023-10-12 23:50:28 +0530

    'update'

--- utils/crop_disease_prediction.py
+++ utils/crop_disease_prediction.py
@@ -1,13 +1,14 @@
 import numpy as np
 from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.vgg16 import preprocess_input
 import json
 from io import BytesIO
 from PIL import Image
 
-potato_disease_model = load_model('./saved_model/Disease_Prediction_Model/potato_disease_model.h5')
-corn_disease_model = load_model('./saved_model/Disease_Prediction_Model/corn_disease_model.h5')
-rice_disease_model = load_model('./saved_model/Disease_Prediction_Model/rice_disease_model.h5')
-wheat_disease_model = load_model('./saved_model/Disease_Prediction_Model/wheat_disease_model.h5')
+potato_disease_model = load_model('./saved_model/Disease_Prediction_Model/vgg16 model/vgg16_potato_disease_model.h5')
+corn_disease_model = load_model('./saved_model/Disease_Prediction_Model/vgg16 model/vgg16_corn_disease_model.h5')
+rice_disease_model = load_model('./saved_model/Disease_Prediction_Model/vgg16 model/vgg16_rice_disease_model.h5')
+wheat_disease_model = load_model('./saved_model/Disease_Prediction_Model/vgg16 model/vgg16_wheat_disease_model.h5')
 
 with open( "./saved_model/Disease_Prediction_Model/crop_disease_mapping.json", 'r') as json_file:
     crop_disease_mapping = json.load(json_file)
@@ -16,22 +17,19 @@ with open( "./saved_model/Disease_Prediction_Model/crop_disease_mapping.json", '
 def get_pred_img(crop_type ,image_file ):
 
 
-
     if (crop_type == 'potato'):
         model = potato_disease_model 
     elif (crop_type == 'corn'):
             model = corn_disease_model 
     elif (crop_type == 'rice'):
-        #     print('select rice crop')
             model = rice_disease_model 
     elif (crop_type == 'wheat'):
-        #     print('select wheat crop')
             model = wheat_disease_model 
 
     # Load and preprocess the uploaded image
     img = Image.open(BytesIO(image_file.read()))
 
-    img = img.resize((256, 256))
+    img = img.resize((224, 224))
 
     # Convert the image to a NumPy array
     img_array = np.array(img)
@@ -39,10 +37,12 @@ def get_pred_img(crop_type ,image_file ):
     img_array = img_array / 225.0
     img_array = np.expand_dims(img_array, axis=0)
 
+    preprocess_img  =  preprocess_input(img_array)
+
     # print(img_array.shape)#(1,64,64,3)
 
    # Make predictions using the model
-    predictions = model.predict(img_array)
+    predictions = model.predict(preprocess_img)
     # print('predictions  , ' , predictions)
     predicted_class = np.argmax(predictions, axis=1)[0]
     
